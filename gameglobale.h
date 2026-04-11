d5f641ffc058ccdc6a6c78589d84de8d251dd1c7
Mohammed-Seyam <msyam1683@gmail.com>
2026-04-11 14:46:36 +0200

    level and platform struct

--- gameglobale.h
+++ gameglobale.h
@@ -12,4 +12,25 @@ using namespace sf;
    const int WINDOW_WIDTH = 1600;
    const int WINDOW_HEIGHT = 900;
    int score = 0;
+
+   struct Vec2{
+    float x, y;
+   };
+
+  //  Level
+   struct Platform{
+    sf::FloatRect rect;
+    bool solid;
+   };
+
+   const int MAX_PLATFORMS = 64;
+
+   struct Level{
+    Platform platforms[MAX_PLATFORMS];
+    int platformCount;
+    sf::Color bgColor;
+    Vec2 spawnPoint;
+   };
+
+   
 #endif
