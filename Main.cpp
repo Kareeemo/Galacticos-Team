200a0d2250d2119210f76331cbc13565ff2f5126
0samaMustafa <om3398244@gmail.com>
2026-04-28 13:12:34 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,58 +1,67 @@
 #include <SFML/Graphics.hpp>
-#include "Physics.h"
-#include "Level.h"
-#include "Render.h"
-#include "Player.h"
-#include "UI.h"
+#include <vector>
+#include <optional>
 #include "gameglobale.h"
+#include "Level.h"
+#include "Player.h"
+#include "Physics.h"
+#include "Render.h"
+#include "UI.h"
 
 int score = 0;
 std::vector<Level> levels;
 
 int main() {
+    sf::RenderWindow window(sf::VideoMode({(unsigned int)WINDOW_WIDTH, (unsigned int)WINDOW_HEIGHT}), "Stick Fight - Galacticos");
+    window.setFramerateLimit(60);
+
     Level currentLevel;
-    Player players[2];
+    int currentLevelId = 0;
+    loadLevel(currentLevel, currentLevelId);
+
+    Player players[MAX_PLAYERS];
     playerInit(players[0], 0, currentLevel);
     playerInit(players[1], 1, currentLevel);
 
-    sf::RenderWindow window(
-        sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}),
-        "Stick Fight - Galacticos Team"
-    );
-    window.setFramerateLimit(60);
     sf::Clock clock;
 
     while (window.isOpen()) {
         float dt = clock.restart().asSeconds();
-        if (dt > 0.05f) dt = 0.05f;
 
-        while (const auto event = window.pollEvent()) {
-            if (event->is<sf::Event::Closed>())
+        while (std::optional<sf::Event> event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
                 window.close();
+            }
         }
 
-        for (int i = 0; i < 2; i++) {
-            playerReadInputForIndex(players[i], i);
-            playerUpdate(players[i], dt);
-            physicsUpdate(players[i], dt);
-        }
+        playerReadInputForIndex(players[0], 0);
+        playerReadInputForIndex(players[1], 1);
+
+        playerUpdate(players[0], dt);
+        playerUpdate(players[1], dt);
+
+        physicsUpdate(players[0], dt);
+        physicsUpdate(players[1], dt);
+
         resolvePlayerCollision(players[0], players[1]);
+
         handleCombat(players[0], players[1]);
         handleCombat(players[1], players[0]);
 
         window.clear(sf::Color::Black);
-        drawBackground(window);
 
-        loadLevel(currentLevel, 1);
+        drawBackground(window);
         drawLevel(window, currentLevel);
 
-        for (int i = 0; i < 2; i++) {
-            drawPlayer(window, players[i], i, dt);
-        }
+        drawPlayer(window, players[0], 0, dt);
+        drawPlayer(window, players[1], 1, dt);
+
+        window.setView(window.getDefaultView());
 
         drawHealthBars(window, players);
 
         window.display();
     }
+
     return 0;
 }
